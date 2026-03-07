78567c36333179ca9c3f55fa6789dbdcf5d06e9e
Md Arman <armanar6712@gmail.com>
2026-03-08 01:55:56 +0530

    Add Shadcn and Tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex items-center justify-center h-screen bg-slate-800">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold uppercase text-slate-400">tasky</h1>
+      <p class="text-slate-300">Personal Task Manager</p>
+    </div>
   </main>
 </template>
